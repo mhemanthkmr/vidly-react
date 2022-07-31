fd3026b83034dcec147bcc93bfa6d3e414842ca6
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-07-31 17:05:34 +0530

    Props Types Implemented

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import _ from "lodash";
 const Pagination = (props) => {
   const { itemsCount, pageSize, onPageChange, currentPage } = props;
@@ -22,5 +23,10 @@ const Pagination = (props) => {
     </nav>
   );
 };
-
+Pagination.propTypes = {
+  itemsCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+};
 export default Pagination;
