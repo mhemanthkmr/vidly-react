a259cca919586e8566e69e6322ab8197855fabc2
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-08-01 14:17:12 +0530

    created table header and working properly

---
+++ src/components/common/tableHeader.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class TableHeader extends Component {
+  state = {};
+  raiseSort = (path) => {
+    const sortColumn = { ...this.props.sortColumn };
+    if (sortColumn.path === path) {
+      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
+    } else {
+      sortColumn.path = path;
+      sortColumn.order = "asc";
+      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
+    }
+    this.props.onSort(sortColumn);
+  };
+  render() {
+    const { columns } = this.props;
+    return (
+      <thead>
+        <tr>
+          {columns.map((column) => (
+            <th
+              key={column.label || column.key}
+              onClick={() => this.raiseSort(column.path)}
+            >
+              {column.label}
+            </th>
+          ))}
+        </tr>
+      </thead>
+    );
+  }
+}
+
+export default TableHeader;
