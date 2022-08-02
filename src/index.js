618f0810b76438a9603a613cc283e0dc921610ca
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-08-02 23:36:05 +0530

    imported app.css in index

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 import "hover.css";
+import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
