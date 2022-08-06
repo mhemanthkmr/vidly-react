ce5bbc10d3e3f57cfc4cfd05e970ad63baaea08d
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-08-06 11:21:19 +0530

    Created movieForms.jsx

---
+++ src/components/movieForms.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const movieForms = (props) => {
+  return (
+    <div className="container">
+      <h1>Movie.id</h1>
+      <button className="btn btn-primary btn-sm">Save</button>
+    </div>
+  );
+};
+
+export default movieForms;
