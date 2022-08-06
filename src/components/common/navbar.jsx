2d70b76106e14df6eb822733eb8d86f4ea252f65
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-08-06 20:53:15 +0530

    Written the navbar content in navbar.jsx

--- src/components/common/navbar.jsx
+++ src/components/common/navbar.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+import { NavLink, Link } from "react-router-dom";
 
 class Navbar extends Component {
   state = {};
   render() {
-    return <h1>Navbar</h1>;
+    return (
+      <nav className="navbar navbar-dark mb-2 navbar-expand-lg bg-dark">
+        <div className="container">
+          <Link className="navbar-brand" to="#">
+            Vidly
+          </Link>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <NavLink className="nav-link" aria-current="page" to="/movies">
+                  Movies
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/rentals">
+                  Rentals
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/customers">
+                  Customers
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
   }
 }
 
