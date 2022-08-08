069ffb98e565995b9ef4dd5e36242ddccf131872
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-08-08 20:59:44 +0530

    Added navbar toggle button

--- src/components/common/navbar.jsx
+++ src/components/common/navbar.jsx
@@ -10,6 +10,17 @@ class Navbar extends Component {
           <Link className="navbar-brand" to="#">
             Vidly
           </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
